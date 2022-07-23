ab9f3d012627a1456d53a7f05d7a3097a4a64506
Luis Paulino <lbpaulino@yahoo.com.br>
2022-07-23 12:54:18 +0000

    acertos dotnet 6.0

---
+++ packages.sh
@@ -0,0 +1,5 @@
+ dotnet add package Microsoft.EntityFrameworkCore --version 5.0.9
+ dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 5.0.9
+ dotnet add package Microsoft.EntityFrameworkCore.Tools --version 5.0.9
+ dotnet add package Swashbuckle.AspNetCore --version 5.6.3
+ dotnet add package EntityFrameworkPaginateCore --version 2.1.0
